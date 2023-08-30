44ab40388880fbab0bd228e5f6326238fb699057
Nicoogf <nicolasgfalabella@gmail.com>
2023-08-30 11:38:59 -0300

    Clase 05 : Ciclo de vida DidMount

---
+++ src/hooks/lifeCycle/DidUpdate.jsx
@@ -0,0 +1,39 @@
+/**
+ * Ejemplo de uso de metedo DidUpdate en componente clase
+ * y Uso de hook en componente Funcional
+*/
+
+import React, { Component  , useEffect } from 'react'
+
+export class DidUpdate extends Component {
+
+    componentDidUpdate(){
+        console.log( "Comportamiento cuando el componente recibe nuevos props o cambio en su estado privado" )
+    }
+
+  render() {
+    return (
+      <div>
+        <h1> DidMount </h1>
+      </div>
+    )
+  }
+}
+
+
+export const DidUpdateHook = () =>{
+
+    // Al no indicar un segundo parametro en el useEfect entre [ ] ,  para que se ejecute cada verz que haya
+    //  un cambio en el estado o props
+
+    useEffect( ()=> {
+        console.log( "Comportamiento cuando el componente recibe nuevos props o cambio en su estado privado" )
+    });
+    
+
+    return (
+        <div>
+          <h1> DidMount </h1>
+        </div>
+    )
+}
