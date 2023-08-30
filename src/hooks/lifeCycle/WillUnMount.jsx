9e374e699a167a6634b96002b479934fd353d7e5
Nicoogf <nicolasgfalabella@gmail.com>
2023-08-30 11:45:22 -0300

    Clase 05 : Ciclo de vida WillUnMount

---
+++ src/hooks/lifeCycle/WillUnMount.jsx
@@ -0,0 +1,47 @@
+/**
+ * Ejemplo de uso del metodo ComponentWillUnMount para componente Clase
+ * Ejemplo de uso del Hook para componente funcional
+ * ( Cuando el componente va a desaparecer )
+ */
+
+import React, { Component, useEffect } from 'react' ;
+
+export class WillUnmount extends Component {
+
+    componentWillUnmount(){
+        console.log( "Comportamiento antes de que el componente desaparezca" )
+    }
+
+  render() {
+    return (
+      <div>
+        <h1> WillUnmount </h1>
+      </div>
+    )
+  }
+}
+
+
+
+export const WillUnMountHook = () => {
+
+    useEffect(() => {
+      // Esta parte no va nada 
+
+      return () => {
+            console.log( "Comportamiento antes de que el componente desaparezca" )
+      }
+    }, []
+     //En esta parte queda el array vacio para decir que se ejecute solo una vez
+);
+
+
+    
+  return (
+    <div>
+        <h1>
+          WillUnmount
+        </h1>
+    </div>
+  )
+}
